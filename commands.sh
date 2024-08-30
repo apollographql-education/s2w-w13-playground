38e0af50ca8f605549c9f236ba6974467c7d3bf9
Michelle Mabuyo <michelle@apollographql.com>
2024-08-30 14:16:39 -0400

    update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,7 +1,5 @@
-# Export variables
-export APOLLO_KEY="..." # Your graph's API key
-export APOLLO_GRAPH_REF="..." # Your graph's unique identifier
-export APOLLO_ROVER_DEV_ROUTER_VERSION=2.0.0-alpha.1 # The version of GraphOS Router that Rover should use for local development
-
-# Run rover dev
+# Run rover dev. Replace ... with your own values
+APOLLO_KEY="..." \ 
+APOLLO_GRAPH_REF="..."  \
+APOLLO_ROVER_DEV_ROUTER_VERSION=2.0.0-alpha.1 \
 rover dev --supergraph-config supergraph-config.yaml --router-config router-config.yaml

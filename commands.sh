8abbdfb8fbcd19918ec2ef40f94a1d0a706c3a24
Michelle Mabuyo <michelle@apollographql.com>
2024-09-03 10:51:39 -0400

    update: router version

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 # Run rover dev. Replace ... with your own values
 APOLLO_KEY="..." \ 
 APOLLO_GRAPH_REF="..."  \
-APOLLO_ROVER_DEV_ROUTER_VERSION=2.0.0-alpha.1 \
+APOLLO_ROVER_DEV_ROUTER_VERSION=2.0.0-alpha.3 \
 rover dev --supergraph-config supergraph-config.yaml --router-config router-config.yaml
